a381ff0e6d23750d3249b50579b4f2a56bd2031b
Lagunaangel195 <lagunaangel896@gmail.com>
2026-03-17 17:12:36 +0000

    Generador de nombres de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
+function generarDominios() {
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  const extensions = ['.com', '.net', '.io', '.us'];
 
+  const lista = document.getElementById("lista");
+  lista.innerHTML = ""; // limpiar lista antes de generar
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+          let dominio = pronoun[i] + adj[j] + noun[k] + extensions[l];
+
+          let li = document.createElement("li");
+          li.textContent = dominio;
+
+          lista.appendChild(li);
+        }
+      }
+    }
+  }
+}
